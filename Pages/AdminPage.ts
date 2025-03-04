f373af082bd8bcd389327316c47bf04f1cc558be
unknown <eslam_shamy@hotmail.com>
2025-03-04 15:43:19 +0200

    admin testcase

--- Pages/AdminPage.ts
+++ Pages/AdminPage.ts
@@ -1,4 +1,4 @@
-import { Page,Locator } from "@playwright/test";
+import { Page,Locator,expect } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 
@@ -17,6 +17,14 @@ export class AdminPage extends BasePage{
     private lastRow: Locator;
     private viewButton: Locator;
     private editButton:Locator;
+    private searchInput:Locator;
+    private resultRow: (email: string) => Locator;
+    private enabledaccount:Locator;
+    private eyeIcon: Locator;
+    private switchButton: Locator;
+    private editstatusButton: Locator;
+
+
     
  
      constructor(page:Page){
@@ -33,7 +41,12 @@ export class AdminPage extends BasePage{
         this.lastRow = page.locator("tbody tr").first()
         this.viewButton = this.lastRow.locator("button.ant-btn-icon-only");
         this.editButton=page.locator('//span[@nztype="edit"]')
-    
+        this.searchInput= page.locator('input[placeholder="بحث عن اسم المشرف أو البريد الإلكتروني"]');
+        this.resultRow = (email: string) =>page.locator('tbody tr').filter({ hasText: email }); 
+        this.enabledaccount=page.locator('tbody tr').filter({ hasText: 'مفعل' }).first();
+        this.eyeIcon=this.enabledaccount.locator('button.ant-btn-icon-only');
+        this.switchButton = page.locator('nz-switch').getByRole('button');
+        this.editstatusButton = page.getByRole('button', { name: 'تعديل' });
         
      }
 
@@ -58,6 +71,34 @@ export class AdminPage extends BasePage{
         await this.adminRoleOption.click()
         await this.saveButton.click()
 
+    }
+
+    async searchByAdminEmail(email: string) {
+        await this.searchInput.fill(email);
+        await this.page.keyboard.press('Enter');
+       
+    }
+    async searchByAdminName(email: string) {
+        await this.searchInput.fill(email);
+        await this.page.keyboard.press('Enter');
+       
+    }
+
+
+
+    async disableAdminAccount(){
+
+        await this.eyeIcon.click();
+        await this.switchButton.click();
+        await this.editstatusButton.click();
+    }
+
+
+    async verifyEmailInResults(email: string) {
+        await expect(this.resultRow(email)).toBeVisible();
+    }
+    async verifyNameInResults(email: string) {
+        await expect(this.resultRow(email)).toBeVisible();
     }
 
 
--- tests/AdminTest.spec.ts
+++ tests/AdminTest.spec.ts
@@ -9,12 +9,15 @@ let landingpage: LandingPage;
 let loginpage: LoginPage;
 let homepage: HomePage;
 let adminpage: AdminPage;
+let email = "admin@tahkeem.com";
+let password = "Changeme@123";
 
 test.beforeEach(async ({ page }) => {
   landingpage = new LandingPage(page);
   loginpage = new LoginPage(page);
   homepage = new HomePage(page);
   adminpage = new AdminPage(page);
+
   await page.goto("/");
 });
 
@@ -34,11 +37,39 @@ test("verify add new admin Arabic name ", async ({ page }) => {
   expect(adminpage.confirmationmessageIsDisplayed).toBeTruthy();
 });
 
-test.only("verify edit admin user", async ({ page }) => {
+test("verify edit admin user", async ({ page }) => {
   await landingpage.goToLoginPage();
   await loginpage.loginAsAdmin("admin@tahkeem.com", "Changeme@123");
   await homepage.navigateToManageAdminPage();
   await adminpage.viewLastUserDetails();
-  await adminpage.editAdmin(faker.person.fullName(), faker.internet.email())
+  await adminpage.editAdmin(faker.person.fullName(), faker.internet.email());
   expect(adminpage.confirmationmessageIsDisplayed).toBeTruthy();
+});
+
+test("verify search by Admin Name", async ({ page }) => {
+  const adminName = "elshamy";
+  await landingpage.goToLoginPage();
+  await loginpage.loginAsAdmin("admin@tahkeem.com", "Changeme@123");
+  await homepage.navigateToManageAdminPage();
+  await adminpage.searchByAdminName(adminName);
+  await adminpage.verifyEmailInResults(adminName);
+});
+
+test("verify search by Admin Email", async ({ page }) => {
+  const adminEmail = "eslameslamelshamy1992@gmail.com	";
+  await landingpage.goToLoginPage();
+  await loginpage.loginAsAdmin("admin@tahkeem.com", "Changeme@123");
+  await homepage.navigateToManageAdminPage();
+  await adminpage.searchByAdminName(adminEmail);
+  await adminpage.verifyEmailInResults(adminEmail);
+});
+
+test.only("verify disable admin account", async ({page}) => {
+  await landingpage.goToLoginPage();
+  await loginpage.loginAsAdmin(email, password);
+  await homepage.navigateToManageAdminPage();
+  await adminpage.disableAdminAccount();
+expect(adminpage.confirmationmessageIsDisplayed).toBeTruthy()
+ 
+  
 });
